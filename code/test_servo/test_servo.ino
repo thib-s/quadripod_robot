9b08c439b3ebafdb056ff7fa4fa2ad93ffef9f3f
thibautBoissin <thibaut@homiwoo.com>
2018-07-12 12:07:33 +0200

    add correction system and values on servos test

--- code/test_servo/test_servo.ino
+++ code/test_servo/test_servo.ino
@@ -3,24 +3,29 @@
 #include <Servo.h>
 
 // lien entre id du servo <=> numero de la pin associée
-int indexes[] = {0,1,2,3,4,5,6,7};
+int indexes[] = { 9, 4, 7, 2, 5, 8, 3, 6};
 Servo servos[8];  // tableau contenant les objet servos
 // heureusement pour nous, un max de 8 objets servos peuvent etre crées
 
 
-int init_angles[] = {90, 90, 90, 60, 90, 60, 90, 90};
+int init_angles[] = {90, 90, 90, 90, 90, 90, 90, 90};
+int correction[] = { -10, 0, 10, 0,  20, 0, -10, 0};
 
 void setup()
 {
+  delay(5000);
   for (int i = 0; i < 8; i++) {
     servos[i].attach(indexes[i]);
   }
-  perform_step(init_angles);
 }
 
 
 void loop()
 {
+  for (int i = 0; i < 8; i++) {
+    servos[i].write(init_angles[i] + correction[i]);
+    delay(1000);
+  }
 }
 
 void perform_step(int angles[]) {
