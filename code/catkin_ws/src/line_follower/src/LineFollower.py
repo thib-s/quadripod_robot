59af33085599c1f964c695a8095dadffb7e8c323
Thibaut Boissin <thibaut.boissin@gmail.com>
2019-10-04 00:50:41 +0200

    added node to mux autopilot and manual pilot

--- code/catkin_ws/src/line_follower/src/LineFollower.py
+++ code/catkin_ws/src/line_follower/src/LineFollower.py
@@ -2,7 +2,7 @@
 
 import sys
 import rospy
-from std_msgs.msg import String, Float32
+from std_msgs.msg import String, Float32, Bool
 from geometry_msgs.msg import Twist
 
 
@@ -11,9 +11,11 @@ class LineFollower():
     def __init__(self):
         self.delta = 0.
         self.theta = 0.
+        self.speed = 1.
         self.smoothing_coef = 0.75  # good at v=1.5
         self.delta_sub = rospy.Subscriber("/plannar_cam/delta", Float32, self.delta_callback)
         self.theta_sub = rospy.Subscriber("/plannar_cam/theta", Float32, self.theta_callback)
+        # self.start_sub = rospy.Subscriber("/pause", Bool, self.start_callback)
         self.delta_coef = 0.05
         self.theta_coef = 0.#.15
 
@@ -23,10 +25,13 @@ class LineFollower():
     def delta_callback(self, msg):
         self.delta = (1- self.smoothing_coef) * self.delta + self.smoothing_coef * msg.data
 
+    # def start_callback(self, msg):
+    #     self.speed = 1. if msg.data else 0.
+
 
 def main(args):
     rospy.init_node('line_follower', anonymous=False)
-    vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+    vel_pub = rospy.Publisher("/nav_vel", Twist, queue_size=1)
     controller = LineFollower()
     vel_pub.publish(Twist())
     rate = rospy.Rate(2)
@@ -34,7 +39,7 @@ def main(args):
     while not rospy.is_shutdown():
         try:
             msg = Twist()
-            msg.linear.x = 1
+            msg.linear.x = controller.speed
             desired = controller.delta_coef * controller.delta +\
                       controller.theta_coef * controller.theta
             msg.angular.z = min(1., max(-1., desired))
